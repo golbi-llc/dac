40b19a378dc34710b93de8d93ecfde129c105b2c
Dinar Davis <dinardavis@gmail.com>
2024-07-20 15:48:27 -0700

    sitewide updates

---
+++ src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import IntroHome from './IntroHome';
+import QuoteHome from './QuoteHome';
+import ApproachHome from "./ApproachHome";
+import ContactHome from './ContactHome';
+import NewsHome from './NewsHome';
+import RightsHome from './RightsHome';
+import PhoneHome from './PhoneHome';
+
+export default function Home() {
+  return (
+    <>
+      <IntroHome />
+      <PhoneHome />
+      <ApproachHome />
+      <RightsHome />
+      <NewsHome />
+      <ContactHome />
+    </>
+  )
+}
